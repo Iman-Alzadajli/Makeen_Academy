1d9cf328b941d8605204189d6a7e00d9986d4b22
safiya Khamis <safia5000@hotmail.com>
2025-05-25 12:05:26 +0400

    added classes, consultation and purchase

---
+++ BackEnd/Makeen_Academy/Models/Consultation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makeen_Academy.Models
+{
+    internal class Consultation
+    {
+        public int Id { get; set; }
+        public string Topic { get; set; }
+        public DateTime Date { get; set; }
+     
+    }
+}
---
+++ BackEnd/Makeen_Academy/Models/Purchase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makeen_Academy.Models
+{
+    internal class Purchase
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public string DeliveryLocation { get; set; }
+    }
+}
