cc085059a29812859b71bb2813bd1c678e5166ad
Ibrahim <Ibrahim@LAPTOP-L47MA11A>
2025-05-25 13:27:04 +0400

    configuration files added for person, customer and trainer

---
+++ BackEnd/Makeen_Academy/Cofiguration/CustomerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Makeen_Academy.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Makeen_Academy.Cofiguration
+{
+    internal class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.ToTable("Customers");
+        }
+    }
+}
